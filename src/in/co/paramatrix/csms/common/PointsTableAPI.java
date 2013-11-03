64330f106f0bcb23426b26110456870c51831da2
bonkeriam <amol.dhane@gmail.com>
2013-11-03 19:09:45 +0530

    Group Related Changes
    Changes done for Tournament with multiple groups

--- src/in/co/paramatrix/csms/common/PointsTableAPI.java
+++ src/in/co/paramatrix/csms/common/PointsTableAPI.java
@@ -1,11 +1,7 @@
 package in.co.paramatrix.csms.common;
 
 import in.co.paramatrix.csms.connection.ConnectionManagerHandler;
-import in.co.paramatrix.csms.dto.MatchInfoDTO;
-import in.co.paramatrix.csms.dto.MatchSummary;
 import in.co.paramatrix.csms.generalamd.GenerateStoreProcedure;
-import in.co.paramatrix.csms.logwriter.LogWriter;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -14,8 +10,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 import javax.servlet.ServletException;
@@ -28,17 +26,23 @@ import sun.jdbc.rowset.CachedRowSet;
 public class PointsTableAPI extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		String strTournamentId = "";
+		if(request.getParameter("tournament") != null){
+			strTournamentId = request.getParameter("tournament");
+		}
 		String strSeasonName = "";
+		String strTournamentName = "";
+		String strSeasonId = "";
+		//String strSeriesTypeId ="";
+		/*
 		if(request.getParameter("season") != null){
 			strSeasonName = request.getParameter("season");
 		}
-		String strTournamentName = "";
 		if(request.getParameter("tournament") != null){
 			strTournamentName = request.getParameter("tournament");
 		}
-		String strSeasonId = "";
-		String strSeriesTypeId ="";
+		*/
 		String strStatus = "SUCCESS";
 		Connection conn = null;
 		Statement stmt = null;
@@ -46,14 +50,14 @@ public class PointsTableAPI extends HttpServlet {
 		String strXmlStatus = "Complete";
 		StringBuilder sbFinalXML = new StringBuilder("<PointTable>");
 		List<Map<String,String>> pointTableList = null;
-		
+
 		try {
-			
+
 			// Get DB connection
 			conn = ConnectionManagerHandler.getConnection("ScoreDB");
 			stmt = conn.createStatement();
-	
-			if(!"".equals(strSeasonName)){
+
+			/*if(!"".equals(strSeasonName)){
 				// Get the season ID for requested season from seasons_mst
 				String strSQL = "SELECT id FROM seasons_mst WHERE status='A' AND name='"+strSeasonName+"'";
 				ResultSet rs = stmt.executeQuery(strSQL);
@@ -62,7 +66,7 @@ public class PointsTableAPI extends HttpServlet {
 				}
 				rs.close();
 			}
-			
+
 			if(!"".equals(strTournamentName)){
 				//Get Series Id for request tournament
 				String strSQL = "Select id from seriestypes_mst WHERE name='" + strTournamentName + "' AND status='A'";
@@ -72,42 +76,186 @@ public class PointsTableAPI extends HttpServlet {
 				}
 				rs.close();
 			}
-			
-			if(!"".equals(strSeasonName) && !"".equals(strTournamentName) && !"".equals(strSeasonId) &&  !"".equals(strSeriesTypeId)){
-				
-				GenerateStoreProcedure lobjGenerateProc = new GenerateStoreProcedure();
-				Vector vparam = new Vector();
-				vparam.add(strSeriesTypeId);
-				vparam.add(strSeasonId);
-			
-				CachedRowSet crsObjGetMatchPt = lobjGenerateProc.GenerateStoreProcedure("esp_dsp_matchpointstally", vparam, "ScoreDB");								
-				vparam.removeAllElements();
-				
-				
-				if(crsObjGetMatchPt != null ){
-					pointTableList = new ArrayList<Map<String,String>>();
-					
-					while(crsObjGetMatchPt.next()){
-						
-						Map<String,String> dataMap = new HashMap<String, String>(12);
-						
-						dataMap.put("team_id", crsObjGetMatchPt.getString("team_id"));
-						dataMap.put("team_name", crsObjGetMatchPt.getString("team_name"));
-						dataMap.put("Played", crsObjGetMatchPt.getString("Played"));
-						dataMap.put("points", crsObjGetMatchPt.getString("points"));
-						dataMap.put("Win", crsObjGetMatchPt.getString("Win"));
-						dataMap.put("Draw", crsObjGetMatchPt.getString("Draw"));
-						dataMap.put("Tie", crsObjGetMatchPt.getString("Tie"));
-						dataMap.put("Aban", crsObjGetMatchPt.getString("abandon"));
-						dataMap.put("Loss", crsObjGetMatchPt.getString("Loss"));
-						dataMap.put("RunRate", crsObjGetMatchPt.getString("RunRate"));
-						dataMap.put("Quotient", crsObjGetMatchPt.getString("Quotient"));
-						
-						pointTableList.add(dataMap);
+			*/
+
+			Map<String,String> seriesMap = null;
+			if(null != strTournamentId && !"".equals(strTournamentId) && !"null".equals(strTournamentId)){
+				try {
+					/*StringBuilder sbSQL = new StringBuilder("SELECT sm.id,stm.name as tournament, stm.id as series_type_id, sn.id as season_id, sn.name as season");
+					sbSQL.append(" FROM series_mst as sm ");
+					sbSQL.append(" INNER JOIN seriestypes_mst as stm ON sm.type = stm.id");
+					sbSQL.append(" INNER JOIN seasons_mst as sn ON sm.season = sn.id");
+					sbSQL.append(" WHERE sm.status='A'");
+					sbSQL.append(" AND stm.status = 'A'");
+					sbSQL.append(" AND sn.status = 'A'");
+					sbSQL.append(" AND sm.id = '"+strTournamentId+"'");
+					ResultSet rs = stmt.executeQuery(sbSQL.toString());
+					if (rs != null && rs.next()) {
+						strSeriesTypeId = rs.getString("series_type_id");
+						strTournamentName = rs.getString("tournament");
+						strSeasonId = rs.getString("season_id");
+						strSeasonName = rs.getString("season");
 					}
-			
-				}
+					rs.close();*/
+					
+					StringBuilder sbSQL = new StringBuilder("SELECT tourn.description as tournament_name,");
+					sbSQL.append(" seriestypes.description as group_name,");
+					sbSQL.append(" series.type as series_type_id,");
+					sbSQL.append(" season.id as season_id,");
+					sbSQL.append(" season.name as season_name");  
+					sbSQL.append(" FROM tournament_series_map as tsm");
+					sbSQL.append(" INNER JOIN tournament_mst as tourn on tourn.id = tsm.tournament_id");
+					sbSQL.append(" INNER JOIN series_mst as series on series.id = tsm.series_id");
+					sbSQL.append(" INNER JOIN seriestypes_mst as seriestypes on seriestypes.id = series.type");
+					sbSQL.append(" INNER JOIN seasons_mst as season on series.season = season.id");
+					sbSQL.append(" WHERE tourn.id = "+strTournamentId);
+					sbSQL.append(" ORDER BY tsm.series_id");
+
+					ResultSet rs = stmt.executeQuery(sbSQL.toString());
+					if (rs != null) {
+						if(rs.next()){
+						
+							//For first Records
+							// Season and Tournament name will be same for all records
+							seriesMap = new HashMap<String, String>();
+							
+							seriesMap.put(rs.getString("series_type_id"), rs.getString("group_name"));
+							strTournamentName = rs.getString("tournament_name");
+							strSeasonId = rs.getString("season_id");
+							strSeasonName = rs.getString("season_name");
+						}
+						while(rs.next()){
+							// For subsequebt records
+							seriesMap.put(rs.getString("series_type_id"), rs.getString("group_name"));
+						}
+					}
+					rs.close();
+
 				
+				} catch (Exception e) {
+					e.printStackTrace();
+					strXmlStatus = "Incomplete";
+					strStatus = "FAILURE";
+				}
+			}
+
+			if(!"".equals(strSeasonName) && !"".equals(strTournamentName) && !"".equals(strSeasonId) &&  seriesMap != null){
+
+				
+				Set<String> seriesTypeSet = seriesMap.keySet();
+				Iterator<String> iter = seriesTypeSet.iterator();
+				
+				sbFinalXML.append(" <Tournament name=");
+				sbFinalXML.append("'"+strTournamentName+"'");
+				//sbFinalXML.append(" id='"+strSeriesTypeId+"'");
+				sbFinalXML.append(" />");
+
+				sbFinalXML.append(" <Season name=");
+				sbFinalXML.append("'"+strSeasonName+"'");
+				sbFinalXML.append(" id='"+strSeasonId+"'");
+				sbFinalXML.append(" />");
+
+				sbFinalXML.append(" <Status msg='" + strStatus + "' />");
+				sbFinalXML.append(" <Groups>");
+				
+				try{
+					while(iter.hasNext()){
+						String strSeriesTypeId =  iter.next();
+						String strGroupName = seriesMap.get(strSeriesTypeId);
+		
+						sbFinalXML.append("<Group name= '"+strGroupName+"' id='"+strSeriesTypeId+"'>");
+						GenerateStoreProcedure lobjGenerateProc = new GenerateStoreProcedure();
+						Vector vparam = new Vector();
+						vparam.add(strSeriesTypeId);
+						vparam.add(strSeasonId);
+		
+						CachedRowSet crsObjGetMatchPt = lobjGenerateProc.GenerateStoreProcedure("esp_dsp_matchpointstally", vparam, "ScoreDB");
+						vparam.removeAllElements();
+		
+						if(crsObjGetMatchPt != null ){
+							
+							while(crsObjGetMatchPt.next()){
+								String strTeamId = "";
+								String strTeamName = "";
+								String strMatchPlayed = "";
+								String strMatchWin = "";
+								String strMatchLost = "";
+								String strMatchTie = "";
+								String strMatchDraw = "";
+								String strMatchAban = "";
+								String strPoints = "";
+								String strNRR = "";
+								String strNR = "";
+								String strQuotient = "";
+								
+								if(crsObjGetMatchPt.getString("team_id") != null){
+									strTeamId = crsObjGetMatchPt.getString("team_id");
+								}
+								if(crsObjGetMatchPt.getString("team_name") != null){
+									strTeamName = crsObjGetMatchPt.getString("team_name");
+								}
+								
+								if(crsObjGetMatchPt.getString("team_id") != null){
+									strTeamId = crsObjGetMatchPt.getString("team_id");
+								}
+								if(crsObjGetMatchPt.getString("team_id") != null){
+									strTeamId = crsObjGetMatchPt.getString("team_id");
+								}
+								if(crsObjGetMatchPt.getString("Played") != null){
+									strMatchPlayed = crsObjGetMatchPt.getString("Played");
+								}
+								if(crsObjGetMatchPt.getString("points") != null){
+									strPoints = crsObjGetMatchPt.getString("points");
+								}
+								if(crsObjGetMatchPt.getString("Win") != null){
+									strMatchWin = crsObjGetMatchPt.getString("Win");
+								}
+								if(crsObjGetMatchPt.getString("Draw") != null){
+									strMatchDraw = crsObjGetMatchPt.getString("Draw");
+								}
+								if(crsObjGetMatchPt.getString("Tie") != null){
+									strMatchTie = crsObjGetMatchPt.getString("Tie");
+								}
+								if(crsObjGetMatchPt.getString("abandon") != null){
+									strMatchAban = crsObjGetMatchPt.getString("abandon");
+								}
+								if(crsObjGetMatchPt.getString("Loss") != null){
+									strMatchLost = crsObjGetMatchPt.getString("Loss");
+								}
+								if(crsObjGetMatchPt.getString("RunRate") != null){
+									strNRR= crsObjGetMatchPt.getString("RunRate");
+								}
+								if(crsObjGetMatchPt.getString("Quotient") != null){
+									strQuotient= crsObjGetMatchPt.getString("Quotient");
+								}
+								
+								sbFinalXML.append(" <Table>");
+								sbFinalXML.append(" <team name=");
+								sbFinalXML.append("'"+strTeamName+"'");
+								sbFinalXML.append(" id='"+strTeamId+"'");
+								sbFinalXML.append("/>");
+	
+								sbFinalXML.append(" <MatchPlayed value='" + strMatchPlayed + "' />");
+								sbFinalXML.append(" <MatchWon value='" + strMatchWin + "' />");
+								sbFinalXML.append(" <MatchLost value='" + strMatchLost + "' />");
+								sbFinalXML.append(" <MatchDraw value='" + strMatchDraw + "' />");
+								sbFinalXML.append(" <MatchTie value='" + strMatchTie + "' />");
+								sbFinalXML.append(" <MatchAban value='" + strMatchAban + "' />");
+								sbFinalXML.append(" <Points value='" + strPoints + "' />");
+								sbFinalXML.append(" <NR value='" + strNR + "' />");
+								sbFinalXML.append(" <NRR value='" + strNRR + "' />");
+								sbFinalXML.append(" <Quotient value='" + strQuotient + "' />");
+								sbFinalXML.append(" </Table>");
+	
+							}
+						}
+						
+						sbFinalXML.append("</Group>");
+					}
+				}
+				finally{
+					sbFinalXML.append(" </Groups>");
+				}
 			}else{
 				strStatus = "FAILURE";
 				strXmlStatus = "Incomplete";
@@ -128,102 +276,10 @@ public class PointsTableAPI extends HttpServlet {
 			stmt = null;
 			conn = null;
 		}
-		
-		sbFinalXML.append(" <Tournament name=");
-		sbFinalXML.append("'"+strTournamentName+"'");
-		sbFinalXML.append(" id='"+strSeriesTypeId+"'");
-		sbFinalXML.append(" />");
-		
-		sbFinalXML.append(" <Season name=");
-		sbFinalXML.append("'"+strSeasonName+"'");
-		sbFinalXML.append(" id='"+strSeasonId+"'");
-		sbFinalXML.append(" />");
-		
-		sbFinalXML.append(" <Status msg='" + strStatus + "' />");
-		
-		//Get data of Point Table
-		if(pointTableList != null && pointTableList.size() > 0){
-			
-			for(Map<String,String> dataMap : pointTableList){
-				if(dataMap != null){
-					
-					String strTeamId = "";
-					String strTeamName = "";
-					String strMatchPlayed = "";
-					String strMatchWin = "";
-					String strMatchLost = "";
-					String strMatchTie = "";
-					String strMatchDraw = "";
-					String strMatchAban = "";
-					String strPoints = "";
-					String strNRR = "";
-					String strNR = "";
-					String strQuotient = "";
-					
-					
-					if(dataMap.get("team_id") != null){
-						strTeamId = dataMap.get("team_id");
-					}
-					if(dataMap.get("team_name") != null){
-						strTeamName = dataMap.get("team_name");
-					}
-					if(dataMap.get("Played") != null){
-						strMatchPlayed = dataMap.get("Played");
-					}
-					if(dataMap.get("points") != null){
-						strPoints = dataMap.get("points");
-					}
-					if(dataMap.get("Win") != null){
-						strMatchWin = dataMap.get("Win");
-					}
-					if(dataMap.get("Draw") != null){
-						strMatchDraw = dataMap.get("Draw");
-					}
-					if(dataMap.get("Tie") != null){
-						strMatchTie = dataMap.get("Tie");
-					}
-					if(dataMap.get("Aban") != null){
-						strMatchAban = dataMap.get("Aban");
-					}
-					if(dataMap.get("Loss") != null){
-						strMatchLost = dataMap.get("Loss");
-					}
-					if(dataMap.get("RunRate") != null){
-						strNRR= dataMap.get("RunRate");
-					}
-					if(dataMap.get("NR") != null){
-						strNR = dataMap.get("NR");
-					}
-					if(dataMap.get("Quotient") != null){
-						strQuotient = dataMap.get("Quotient");
-					}
-					
-					sbFinalXML.append(" <Table>");
-					sbFinalXML.append(" <team name=");
-					sbFinalXML.append("'"+strTeamName+"'");
-					sbFinalXML.append(" id='"+strTeamId+"'");
-					sbFinalXML.append("/>");
-					
-					sbFinalXML.append(" <MatchPlayed value='" + strMatchPlayed + "' />");
-					sbFinalXML.append(" <MatchWon value='" + strMatchWin + "' />");
-					sbFinalXML.append(" <MatchLost value='" + strMatchLost + "' />");
-					sbFinalXML.append(" <MatchDraw value='" + strMatchDraw + "' />");
-					sbFinalXML.append(" <MatchTie value='" + strMatchTie + "' />");
-					sbFinalXML.append(" <MatchAban value='" + strMatchAban + "' />");
-					sbFinalXML.append(" <Points value='" + strPoints + "' />");
-					sbFinalXML.append(" <NR value='" + strNR + "' />");
-					sbFinalXML.append(" <NRR value='" + strNRR + "' />");
-					sbFinalXML.append(" <Quotient value='" + strQuotient + "' />");
-					sbFinalXML.append(" </Table>");
-				}
-			}
-			
-			//sbFinalXML.append(" </Table>");
-		}
-		
+
 		sbFinalXML.append("<xmlstatus>"+strXmlStatus + "</xmlstatus>");
 		sbFinalXML.append(" </PointTable>");
-		
+
 		strFinalXML = sbFinalXML.toString().replace("&", " and ");
 		String output = new String(strFinalXML.getBytes("UTF-8"), "ISO-8859-1");
 		response.setContentType("text/xml");
